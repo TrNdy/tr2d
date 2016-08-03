cef4e2d4f1483ec0eb282609412f9c939c02f1e4
Florian Jug <jug@mpi-cbg.de>
2016-08-03 19:05:15 +0200

    Updated plugin to ImageJ2 type plugin

--- src/main/java/Tr2dPlugin.java
+++ src/main/java/Tr2dPlugin.java
@@ -1,44 +1,26 @@
+import org.scijava.command.Command;
+import org.scijava.plugin.Plugin;
+
 import com.indago.app.hernan.Tr2dApplication;
 
-import ij.IJ;
 import ij.ImagePlus;
-import ij.plugin.PlugIn;
 
 /**
- * tr2d
+ * Tr2d Plugin for Fiji/ImageJ2
  *
  * @author Florian Jug
  */
-public class tr2d_ implements PlugIn {
+
+@Plugin( type = Command.class, headless = true, menuPath = "Plugins>Tracking>Tr2d (alpha)" )
+public class Tr2dPlugin implements Command {
 	protected ImagePlus image;
 
 	/**
-	 * @see ij.plugin.PlugIn#run(java.lang.String)
+	 * @see java.lang.Runnable#run()
 	 */
 	@Override
-	public void run( final String arg ) {
+	public void run() {
+		Tr2dApplication.isStandalone = false;
 		Tr2dApplication.main( null );
 	}
-
-	/**
-	 * Main method for debugging.
-	 *
-	 * For debugging, it is convenient to have a method that starts ImageJ,
-	 * loads an
-	 * image and calls the plugin, e.g. after setting breakpoints.
-	 *
-	 * @param args
-	 *            unused
-	 */
-	public static void main(final String[] args) {
-		// set the plugins.dir property to make the plugin appear in the Plugins menu
-		final Class<?> clazz = tr2d_.class;
-		final String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
-		final String pluginsDir = url.substring(5, url.length() - clazz.getName().length() - 6);
-		System.setProperty("plugins.dir", pluginsDir);
-
-		// run the plugin
-		IJ.runPlugIn(clazz.getName(), "");
-	}
-
 }
