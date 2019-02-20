85374afbef1fba2abffc5beb6ab595254aee01d8
mp007 <praka027@umn.edu>
2019-02-20 17:21:27 +0100

    WIP: Starting implementtaion of TRA compatibel image generation

--- src/main/java/com/indago/tr2d/pg/Tr2dTrackingProblem.java
+++ src/main/java/com/indago/tr2d/pg/Tr2dTrackingProblem.java
@@ -482,6 +482,15 @@ public class Tr2dTrackingProblem implements TrackingProblem {
 			}
 		}
 
+//		public void saveTRACompatibleResult( final Tr2dTrackingProblem ttp ) {
+//			long x = ttp.trackingModel.getImgSolution().dimension( 0 );
+//			long y = ttp.trackingModel.getImgSolution().dimension( 1 );
+//			for (final Tr2dSegmentationProblem t : ttp.getTimepoints()) {
+//				RandomAccessibleInterval< IntType > blankImg = new Rand;
+//			}
+//		}
+
+
 		public void saveSolution( final Tr2dTrackingProblem ttp, final Assignment< IndicatorNode > pgAssignment, final File file ) {
 			try {
 				final BufferedWriter solutionWriter = new BufferedWriter( new FileWriter( file ) );
